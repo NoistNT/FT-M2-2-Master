d3cfbb74d016e265e7ce85985bc3aa236abba510
NoistNT <arielgnr23@gmail.com>
2023-03-10 00:57:36 -0300

    Complete homework

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
 export default class Animals extends React.Component {
-  constructor (props) {
-    super()
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props)
   }
 
-  render () {
-    return <div></div>
+  render() {
+    return (
+      <div>
+        {this.props.animals.map(({ index, name, image, specie }) => {
+          return (
+            <div key={index}>
+              <hr />
+              <h5>{name}</h5>
+              <img src={image} alt={name} width="300px" />
+              <br />
+              <span>{specie}</span>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Species () {
+export default function Species({ species, handleAllSpecies, handleSpecies }) {
   return (
     <div>
-
+      <h2>Species</h2>
+      {species.map((specie, index) => (
+        <button key={index} value={specie} onClick={handleSpecies}>
+          {specie}
+        </button>
+      ))}
+      <button onClick={handleAllSpecies}>All Animals</button>
     </div>
   )
 }
